594c6af3e43d721abb574949333f8a8a04d996eb
Mad Lad <madeleine.day@hotmail.com>
2021-02-22 08:37:46 +1300

    Backend initial commit

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            // Add students here
+        };
+    }
+}
